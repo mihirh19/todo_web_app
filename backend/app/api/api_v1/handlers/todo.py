8f46131b5a1c521bbc3cf6b30456cb3beb611a46
mihirh19 <miheerhadvani990@gmail.com>
2023-04-01 13:40:04 +0530

    ✅Backend API Final with `__init__.py`

--- backend/app/api/api_v1/handlers/todo.py
+++ backend/app/api/api_v1/handlers/todo.py
@@ -31,6 +31,6 @@ async def update(todo_id: UUID, data: TodoUpdate, current_user: User = Depends(g
 
 
 @todo_router.delete('/{todo_id}', summary="delete todo by todo_id")
-async def delete(todo_id:UUID, current_user: User = Depends(get_current_user) ):
-    await TodoService.delete_todo(current_user,todo_id)
+async def delete(todo_id: UUID, current_user: User = Depends(get_current_user)):
+    await TodoService.delete_todo(current_user, todo_id)
     return None
